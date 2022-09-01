420816e56782ca5b45df7cf4a4b8af72983a89f6
NascimentoTales <nascimentosantostales@gmail.com>
2022-08-31 23:36:42 -0300

    iniciando

---
+++ src/scripts/script.js
@@ -0,0 +1,29 @@
+const squares = document.querySelector(".squares")
+
+console.log(squares);
+
+for (let i = 0; i < 10; i++) {
+    
+    let li = document.createElement('li')
+    const random = (max,min) => Math.random()*(max - min) + min;
+
+    const size = Math.floor(random(120, 40))
+    const position = Math.floor(random(100, 0))
+    const delay = Math.floor(random(10,0.5))
+    let duration = Math.floor(random(12, 6));
+
+    li.style.width = `${size}px`
+    li.style.height = `${size}px`
+    li.style.bottom = -`${size}px`
+
+    li.style.left = `${position}%`
+
+    li.style.animationDelay = `${delay}s`
+    li.style.animationDuration = `${duration}s`
+    // li.style.animationFillMode = `cubic-bezier(${Math.random}, ${Math.random}, ${Math.random}, ${Math.random})`
+
+    squares.appendChild(li)
+    console.log(li);
+    document.querySelector("body").style.overflow = "hidden"
+
+}
